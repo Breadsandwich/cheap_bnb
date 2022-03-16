e867ff995584638df2ae29b65b4bd095a866d047
daniel <danielthai14@gmail.com>
2022-03-16 01:07:39 -0700

    finished writing routes for booking and reviews, needs seeders and testing

--- app/api/booking_routes.py
+++ app/api/booking_routes.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Blueprint, request
 from flask_login import login_required, current_user
 import psycopg2
@@ -20,15 +21,70 @@ def validation_errors_to_error_messages(validation_errors):
 
 # -- create bookings --
 @booking_routes.route('/new', methods=['POST'])
-@login_required
+# @login_required
 def create_booking():
-    data = request.json
-    form = BookingForm()
+  form = BookingForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+
+  if form.validate_on_submit():
+    new_booking = Booking(
+      user_id = form.data['user_id'],
+      spot_id = form.data['spot_id'],
+      start_date = form.data['start_date'],
+      end_date = form.data['end_date'],
+      guests = form.data['guests'],
+      user_id = form.data['user_id'],
+      user_id = form.data['user_id'],
+      user_id = form.data['user_id'],
+      created_at = datetime.now(),
+      updated_at = datetime.now()
+    )
+
+    db.session.add(new_booking)
+    db.session.commit()
+
+    return { 'booking': new_booking.to_dict()}
 
 
 
 # -- read bookings --
+@booking_routes.routes('/<int:id>', methods=['GET'])
+# @login_required
+def get_bookings(id):
+  all_my_bookings = Booking.query.filter(Booking.user_id == id).all()
+
+  return {'all_my_bookings': [booking.to_dict() for booking in all_my_bookings]}
 
 # -- update bookings --
+@booking_routes.route('/<int:id>', methods=['PUT'])
+# @login_required
+def update_booking():
+  form = BookingForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+
+  if form.validate_on_submit():
+    booking = Booking.query.get(id)
+    booking.user_id = form.data['user_id']
+    booking.spot_id = form.data['spot_id']
+    booking.start_date = form.data['start_date']
+    booking.end_date = form.data['end_date']
+    booking.guests = form.data['guests']
+    booking.updated_at = datetime.now()
+
+    db.session.commit()
+
+    return { 'booking': booking.to_dict() }
+
+  return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
 
 # -- delete bookings --
+
+@booking_routes.route("/<int:bookingId>", methods=['DELETE'])
+@login_required
+def delete_booking(bookingId):
+  booking = Booking.query.get(bookingId)
+  db.session.delete(booking)
+  db.session.commit()
+
+  return {'bookingId': bookingId}
--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -18,10 +18,10 @@ def validation_errors_to_error_messages(validation_errors):
 
 # CRUD routes
 
-# -- create bookings --
+# -- create reviews --
 @review_routes.route('/new', methods=['POST'])
 @login_required
-def create_booking():
+def create_review():
     data = request.json
     form = ReviewForm()
     form['csrf_token'].data = request.cookies['csrf_token']
@@ -29,7 +29,7 @@ def create_booking():
     if form.validate_on_submit():
         new_review = Review(
             user_id = data['user_id'],
-            spot_id = data['spot_id']
+            spot_id = data['spot_id'],
             review = data['review'],
             rating = data['rating'],
             created_at = datetime.now(),
@@ -43,14 +43,39 @@ def create_booking():
 
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
-# -- read bookings --
-@review_routes.route('/<int:spotId>', methods=['GET'])
-def get_reviews(spotId):
-  all_reviews = Review.query.filter(Review.spot_id == int(spotId)).all()
+# -- read reviews --
+@review_routes.route('/<int:id>', methods=['GET'])
+def get_reviews(id):
+  all_reviews = Review.query.filter(Review.spot_id == int(id)).all()
 
   return {"spot_reviews": [review.to_dict() for review in all_reviews]}
 
-# -- update bookings --
+# -- update reviews --
+@review_routes.route("/<int:id>", methods=['PUT'])
+# @login_required
+def update_review(id):
+    data = request.json
+    form = ReviewForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+      updated_review = Review.query.get(id)
+      updated_review.rating = data['rating']
+      updated_review.review = data['review']
+
+      db.session.commit()
+
+      return { 'review': update_review.to_dict() }
+
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
-# -- delete bookings --
+# -- delete reviews --
+@review_routes.route("/<int:reviewId>", methods=['DELETE'])
+@login_required
+def delete_review(reviewId):
+  review = Review.query.get(reviewId)
+  db.session.delete(review)
+  db.session.commit()
+
+  return {'reviewId': reviewId}
